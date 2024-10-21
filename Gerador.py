f7679c1401d3dade0ff23256df319628353abe70
mendesjota <mendesjunior_@hotmail.com>
2024-10-20 22:48:59 -0300

    Update Gerador.py

--- Gerador.py
+++ Gerador.py
@@ -6708,8 +6708,13 @@ if promo_type == "Volume":
     desconto_percentual_disabled = False
     ttv_fixo_disabled = False
 
-    # Verifica se um campo já foi preenchido
+    # Criando colunas para exibir os campos lado a lado
+col1, col2 = st.columns(2)
+
+with col1:
     desconto_percentual = st.text_input("Desconto Percentual (0 a 1.0)", key="desconto_percentual", disabled=ttv_fixo_disabled)
+
+with col2:
     ttv_fixo = st.text_input("TTV Fixo", key="ttv_fixo", disabled=desconto_percentual != "")
 
            
